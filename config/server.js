89c44cc9cc4b015b3560da02e82fe6c2fe0d97d0
taha-fatima <tahafatima671@gmail.com>
2021-09-04 06:46:53 +0530

    test

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8df3144e43707f736c550df17e161f97'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e20af87c-646b-44b9-b6b3-1eb1ea088b0f'
+};
